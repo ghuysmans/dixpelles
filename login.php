78f3cb35f16c742ddf45eba0f9ec82c7c8b34625
Guillaume Huysmans <ghuysmans99@gmail.com>
2022-02-07 00:36:09 +0100

    Reuse the sample login form

--- login.php
+++ login.php
@@ -9,6 +9,7 @@ if (isset($_POST['email']) && isset($_POST['pass'])) {
 	if ($r = $q->fetch()) {
 		$_SESSION['user'] = $r['id'];
 		$_SESSION['admin'] = $r['admin'];
+		//TODO handle $_POST['rem']
 		header('Location: index.php');
 		exit();
 	}
@@ -27,9 +28,19 @@ if ($failed)
 	show_alert('danger', 'E-mail et/ou mot de passe incorrect(s)');
 ?>
 <form method="post">
-<label>E-mail : <input type="email" name="email"></label>
-<label>Mot de passe : <input type="password" name="pass"></label>
-<button>Connexion</button>
+  <div class="mb-3">
+    <label class="form-label" for="email">Adresse e-mail</label>
+    <input type="email" class="form-control" name="email" id="email" required>
+  </div>
+  <div class="mb-3">
+    <label for="pass" class="form-label">Mot de passe</label>
+    <input type="password" class="form-control" name="pass" id="pass" required>
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" name="rem" id="rem">
+    <label class="form-check-label" for="rem">Se souvenir de moi</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Connexion</button>
 </form>
 <?php
 show_footer();
